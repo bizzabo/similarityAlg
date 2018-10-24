99f93dd4cba35306c01329895758ad534bbcd143
lior abramov <liora1443@gmail.com>
2018-10-24 21:46:04 +0300

    add id to steps

--- logic.py
+++ logic.py
@@ -1,8 +1,5 @@
 import math
 
-
-
-
 industryMap = {
     'MARKETING_AND_ADVERTISING': 1,
     'INTERNET': 2,
@@ -20,22 +17,21 @@ roleMap = {
     'Generic': 6
 }
 
-
-data3 ={
+data3 = {
     'Lior Plis': {
-		'Role': 1.0,
-		'Age':0.25,
-		'Experience': 3.34,
-		'Industry': 2.32
-	},
-	'Jhonny Per': {
+        'Role': 1.0,
+        'Age': 0.25,
+        'Experience': 3.34,
+        'Industry': 2.32
+    },
+    'Jhonny Per': {
         'Role': 2.0,
         'Age': 0.36,
         'Experience': 2.1,
         'Industry': 3.0,
-        'flow':1
+        'flow': 1
     },
-	'Itamr Ret': {
+    'Itamr Ret': {
         'Role': 3.0,
         'Age': 0.45,
         'Experience': 3.2,
@@ -43,7 +39,7 @@ data3 ={
         'flow': 4
 
     },
-	'Tamir Der': {
+    'Tamir Der': {
         'Role': 2.0,
         'Age': 0.30,
         'Experience': 3.88,
@@ -51,69 +47,67 @@ data3 ={
         'flow': 4
 
     },
-	'Tamir2 Der2': {
+    'Tamir2 Der2': {
         'Role': 2.0,
-        'Age':0.50,
+        'Age': 0.50,
         'Experience': 3.88,
         'Industry': 2.55,
         'flow': 5
     },
-	'Marvin Minsky': {
-		'Role': 5.0,
-		'Age': 0.23,
-		'Experience': 4.32,
-		'Industry': 2.76,
+    'Marvin Minsky': {
+        'Role': 5.0,
+        'Age': 0.23,
+        'Experience': 4.32,
+        'Industry': 2.76,
         'flow': 4
-	},
+    },
     'John McCarthy': {
-		'Role': 3.0,
-		'Age': 0.58,
-		'Experience': 3.23,
-		'Industry': 3.03,
+        'Role': 3.0,
+        'Age': 0.58,
+        'Experience': 3.23,
+        'Industry': 3.03,
         'flow': 4
-	},
+    },
     'Edsger Dijkstra': {
-		'Role': 5.0,
-		'Age': 0.52,
-		'Experience': 3.04,
-		'Industry': 4.97,
-		'flow': 5
+        'Role': 5.0,
+        'Age': 0.52,
+        'Experience': 3.04,
+        'Industry': 4.97,
+        'flow': 5
 
-	},
+    },
 
-	'Donald Knuth': {
-		'Role': 1.0,
-		'Age': 0.52,
-		'Experience': 5.0,
-		'Industry': 1.97,
-		'flow': 4
-	},
+    'Donald Knuth': {
+        'Role': 1.0,
+        'Age': 0.52,
+        'Experience': 5.0,
+        'Industry': 1.97,
+        'flow': 4
+    },
     'John Backus': {
-		'Role': 5.0,
-		'Age': 0.40,
-		'Experience': 4.3,
-		'Industry': 3.2,
-		'flow': 3
-	},
-
-	'Robert Floyd': {
-		'Role': 4.0,
-		'Age': 0.56,
-		'Experience': 4.04,
-		'Industry': 3.97,
-        'flow': 2
-	},
-
-	'Tony Hoare': {
-		'Role': 4.0,
-		'Age': 0.28,
-		'Experience': 4.2,
-		'Industry': 3.07,
+        'Role': 5.0,
+        'Age': 0.40,
+        'Experience': 4.3,
+        'Industry': 3.2,
         'flow': 3
-	},
+    },
+
+    'Robert Floyd': {
+        'Role': 4.0,
+        'Age': 0.56,
+        'Experience': 4.04,
+        'Industry': 3.97,
+        'flow': 2
+    },
+
+    'Tony Hoare': {
+        'Role': 4.0,
+        'Age': 0.28,
+        'Experience': 4.2,
+        'Industry': 3.07,
+        'flow': 3
+    },
 }
-
-
 
 
 def euclidean_similarity(person1, person2):
@@ -122,6 +116,7 @@ def euclidean_similarity(person1, person2):
     rankings = [(data[person1][itm], data[person2][itm]) for itm in common_ranked_items]
     distance = [pow(rank[0] - rank[1], 2) for rank in rankings]
     return 1 / (1 + sum(distance))
+
 
 def pearson_similarity(person1, person2):
     data = data3
@@ -140,6 +135,7 @@ def pearson_similarity(person1, person2):
     den = math.sqrt((n * ss1 - math.pow(s1, 2)) * (n * ss2 - math.pow(s2, 2)))
 
     return (num / den) if den != 0 else 0
+
 
 def replace_models_data(model_dict):
     index = 0
@@ -161,7 +157,8 @@ def keyToValue(key, value):
     if (key == 'Industry'):
         return industryMap[value]
 
-def recommend(person,dataObj, bound, similarity=pearson_similarity):
+
+def recommend(person, dataObj, bound, similarity=pearson_similarity):
     personObj = replace_models_data(dataObj)
     data = data3
     data[person] = personObj
@@ -170,65 +167,68 @@ def recommend(person,dataObj, bound, similarity=pearson_similarity):
     scores.reverse()
     scores = scores[0:bound]
 
-    #print (scores)
+    # print (scores)
 
     recomms = {}
 
     for sim, other in scores:
-     ranked = data[other]
+        ranked = data[other]
 
     for itm in ranked:
-            if itm not in data[person]:
-                weight = sim * ranked[itm]
+        if itm not in data[person]:
+            weight = sim * ranked[itm]
 
-                if itm in recomms:
-                    s, weights = recomms[itm]
-                    recomms[itm] = (s + sim, weights + [weight])
-                else:
-                    recomms[itm] = (sim, [weight])
+            if itm in recomms:
+                s, weights = recomms[itm]
+                recomms[itm] = (s + sim, weights + [weight])
+            else:
+                recomms[itm] = (sim, [weight])
 
     for r in recomms:
         sim, item = recomms[r]
         recomms[r] = sum(item) / sim
 
-    t=flowMap[(str(int(recomms['flow'])))],recomms['flow']
-    return t
+    flowId = int(recomms['flow'])
+    flowList = flowMap[flowId]
+    flowDict = [{stepMap[f]: f} for f in flowList]
+
+    return {flowId: flowDict}
 
 
-x={
-"Role": 'Marketer',
-"Age":30,
-"Experience": 5,
-"Industry": 'MARKETING_AND_ADVERTISING'
+x = {
+    "Role": 'Marketer',
+    "Age": 30,
+    "Experience": 5,
+    "Industry": 'MARKETING_AND_ADVERTISING'
 }
 
 flowMap = {
     # Content Manager flow
-    '1':
+    1:
         ['https://accounts.bizzabo.com/{accountId}/events/{eventId}/info/general',
-          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/speakers/manage',
-          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/manageTags',
-          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/locations',
-          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/editor',
-          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/registration/sessionRegistration',
-          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/website/settings'],
+         'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/speakers/manage',
+         'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/manageTags',
+         'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/locations',
+         'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/editor',
+         'https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/registration/sessionRegistration',
+         'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/website/settings'],
 
     # Designer
-    '2':
+    2:
         ['https://accounts.bizzabo.com/preview/{accountId}/events/{eventId}',
          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/app',
          'https://accounts.bizzabo.com/{accountId}/mobileApp']
     ,
 
     # Contact/Registration Manager
-    '3':
+    3:
         ['https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/setup/create',
          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/registration/form',
          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/registration/confirmation',
          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/promos'],
 
     # Admin
-    '4':
+    4:
         ['https://accounts.bizzabo.com/{accountId}/team',
          'https://accounts.bizzabo.com/{accountId}/payment/processor',
          'https://accounts.bizzabo.com/{accountId}/customEmail',
@@ -236,7 +236,7 @@ flowMap = {
          'https://accounts.bizzabo.com/{accountId}/account/subscriptionUsage'],
 
     # Marketing Exec.
-    '5':
+    5:
         ['https://accounts.bizzabo.com/132427/events/{eventId}/contacts/contacts',
          'https://accounts.bizzabo.com/132427/events/{eventId}/promote/ticketBoost',
          'https://accounts.bizzabo.com/132427/events/{eventId}/contacts/emailAttendee',
@@ -245,16 +245,42 @@ flowMap = {
     ,
 
     # Generic User
-    '6':
+    6:
         ['https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/setup/create',
          'https://accounts.bizzabo.com/preview/{accountId}/events/{eventId}',
          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/registration/form',
          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/editor',
          'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/website/settings']
 
+}
 
+stepMap = {
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/info/general': 1,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/speakers/manage': 2,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/manageTags': 3,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/locations': 4,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/agenda/editor': 5,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/registration/sessionRegistration': 6,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/website/settings': 7,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/setup/create': 8,
+    'https://accounts.bizzabo.com/preview/{accountId}/events/{eventId}': 9,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/registration/form': 10,
+    'https://accounts.bizzabo.com/132427/events/{eventId}/contacts/contacts': 11,
+    'https://accounts.bizzabo.com/132427/events/{eventId}/promote/ticketBoost': 12,
+    'https://accounts.bizzabo.com/132427/events/{eventId}/contacts/emailAttendee': 13,
+    'https://accounts.bizzabo.com/132427/events/{eventId}/promote/socialShare': 14,
+    'https://accounts.bizzabo.com/132427/events/{eventId}/apps/website/trackingPixel': 15,
+    'https://accounts.bizzabo.com/{accountId}/team': 16,
+    'https://accounts.bizzabo.com/{accountId}/payment/processor': 17,
+    'https://accounts.bizzabo.com/{accountId}/customEmail': 18,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/reports/registrations': 19,
+    'https://accounts.bizzabo.com/{accountId}/account/subscriptionUsage': 20,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/apps/app': 21,
+    'https://accounts.bizzabo.com/{accountId}/mobileApp': 22,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/tickets/registration/confirmation': 23,
+    'https://accounts.bizzabo.com/{accountId}/events/{eventId}/promos': 24
 }
 
 #list = recommend('name', x, 5)
 #for itm in list:
-#    print(itm)
+    #print(itm)
